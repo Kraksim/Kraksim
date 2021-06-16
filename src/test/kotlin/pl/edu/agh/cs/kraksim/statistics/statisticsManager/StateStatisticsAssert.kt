c56df0bb0efb3fff5a49b45df4559b5e9e31ac70
WikKam <kamykikamienie@gmail.com>
2021-06-16 19:46:51 +0200

    review fixesx2

--- src/test/kotlin/pl/edu/agh/cs/kraksim/statistics/statisticsManager/StateStatisticsAssert.kt
+++ src/test/kotlin/pl/edu/agh/cs/kraksim/statistics/statisticsManager/StateStatisticsAssert.kt
@@ -1,6 +1,6 @@
 package pl.edu.agh.cs.kraksim.statistics.statisticsManager
 
-import org.assertj.core.api.AssertionsForClassTypes
+import org.assertj.core.api.Assertions.assertThat
 import pl.edu.agh.cs.kraksim.common.AverageSpeed
 import pl.edu.agh.cs.kraksim.common.Density
 import pl.edu.agh.cs.kraksim.common.FlowRatio
@@ -10,50 +10,50 @@ import pl.edu.agh.cs.kraksim.statistics.StateStatistics
 class StateStatisticsAssert(private val stateStatistics: StateStatistics) {
     fun assertCurrentDensity(roadId: RoadId, density: Density): StateStatisticsAssert {
         val currentDensity = stateStatistics.currentStatisticsValues.density[roadId]
-        AssertionsForClassTypes.assertThat(currentDensity).isEqualTo(density)
+        assertThat(currentDensity).isEqualTo(density)
         return this
     }
 
     fun assertCurrentFlowRatio(roadId: RoadId, flowRatio: FlowRatio?): StateStatisticsAssert {
         val roadFlowRatio = stateStatistics.currentStatisticsValues.roadFlowRatio[roadId]
-        AssertionsForClassTypes.assertThat(roadFlowRatio).isEqualTo(flowRatio)
+        assertThat(roadFlowRatio).isEqualTo(flowRatio)
         return this
     }
 
     fun assertCurrentRoadAverageSpeed(roadId: RoadId, averageSpeed: AverageSpeed?): StateStatisticsAssert {
         val roadAverageSpeed = stateStatistics.currentStatisticsValues.speedStatistics.roadAverageSpeed[roadId]
-        AssertionsForClassTypes.assertThat(roadAverageSpeed).isEqualTo(averageSpeed)
+        assertThat(roadAverageSpeed).isEqualTo(averageSpeed)
         return this
     }
 
     fun assertCurrentWholeMapAverageSpeed(averageSpeed: AverageSpeed?): StateStatisticsAssert {
         val wholeMapAverageSpeed = stateStatistics.currentStatisticsValues.speedStatistics.wholeMapAverageSpeed
-        AssertionsForClassTypes.assertThat(wholeMapAverageSpeed).isEqualTo(averageSpeed)
+        assertThat(wholeMapAverageSpeed).isEqualTo(averageSpeed)
         return this
     }
 
     fun assertTotalDensity(roadId: RoadId, density: Density): StateStatisticsAssert {
         val currentDensity = stateStatistics.totalStatisticsValues.density[roadId]
-        AssertionsForClassTypes.assertThat(currentDensity).isEqualTo(density)
+        assertThat(currentDensity).isEqualTo(density)
         return this
     }
 
 
     fun assertTotalFlowRatio(roadId: RoadId, flowRatio: FlowRatio?): StateStatisticsAssert {
         val roadFlowRatio = stateStatistics.totalStatisticsValues.roadFlowRatio[roadId]
-        AssertionsForClassTypes.assertThat(roadFlowRatio).isEqualTo(flowRatio)
+        assertThat(roadFlowRatio).isEqualTo(flowRatio)
         return this
     }
 
     fun assertTotalRoadAverageSpeed(roadId: RoadId, averageSpeed: AverageSpeed?): StateStatisticsAssert {
         val roadAverageSpeed = stateStatistics.totalStatisticsValues.speedStatistics.roadAverageSpeed[roadId]
-        AssertionsForClassTypes.assertThat(roadAverageSpeed).isEqualTo(averageSpeed)
+        assertThat(roadAverageSpeed).isEqualTo(averageSpeed)
         return this
     }
 
     fun assertTotalWholeMapAverageSpeed(averageSpeed: AverageSpeed?): StateStatisticsAssert {
         val wholeMapAverageSpeed = stateStatistics.totalStatisticsValues.speedStatistics.wholeMapAverageSpeed
-        AssertionsForClassTypes.assertThat(wholeMapAverageSpeed).isEqualTo(averageSpeed)
+        assertThat(wholeMapAverageSpeed).isEqualTo(averageSpeed)
         return this
     }
 }
