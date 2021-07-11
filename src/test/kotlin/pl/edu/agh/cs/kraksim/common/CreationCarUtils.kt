8ae1b95b92de0482e7ab248a33859795849d3dea
WikKam <kamykikamienie@gmail.com>
2021-07-11 18:43:50 +0200

    removed unused imports

--- src/test/kotlin/pl/edu/agh/cs/kraksim/common/CreationCarUtils.kt
+++ src/test/kotlin/pl/edu/agh/cs/kraksim/common/CreationCarUtils.kt
@@ -3,8 +3,6 @@ package pl.edu.agh.cs.kraksim.common
 import pl.edu.agh.cs.kraksim.common.gps.MockRoadGps
 import pl.edu.agh.cs.kraksim.core.state.Car
 import pl.edu.agh.cs.kraksim.nagelCore.state.NagelCar
-import java.util.stream.IntStream
-import kotlin.streams.asSequence
 
 fun createListOfCars(amount: Int, velocity: Int, spaces: Int): List<Car> {
     return (0 until amount)
