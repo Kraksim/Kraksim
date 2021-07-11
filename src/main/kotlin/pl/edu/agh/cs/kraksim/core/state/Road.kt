8853314e5c2e671beac066de7cb2ea33ef72ba01
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-07-11 17:12:05 +0200

    some poggers3

--- src/main/kotlin/pl/edu/agh/cs/kraksim/core/state/Road.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/core/state/Road.kt
@@ -1,11 +1,9 @@
 package pl.edu.agh.cs.kraksim.core.state
 
-import pl.edu.agh.cs.kraksim.nagelCore.state.NagelRoadNode
-
 interface Road {
     val id: Long
     val physicalLength: Int
     val lanes: List<Lane>
 
-    val end: NagelRoadNode
+    val end: RoadNode
 }
