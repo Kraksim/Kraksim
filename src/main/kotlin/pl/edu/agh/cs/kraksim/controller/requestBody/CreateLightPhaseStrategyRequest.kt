c1de83bd8a36f9da259d022be69cb1ef6f4ed895
Wiktor Kamiński <wkaminski@qualtrics.com>
2021-10-12 19:55:30 +0200

    smiecenie gitloga cd, wyebanie publicow w pizduuuuuu

--- src/main/kotlin/pl/edu/agh/cs/kraksim/controller/requestBody/CreateLightPhaseStrategyRequest.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/controller/requestBody/CreateLightPhaseStrategyRequest.kt
@@ -4,7 +4,7 @@ import pl.edu.agh.cs.kraksim.common.IntersectionId
 import pl.edu.agh.cs.kraksim.repository.entities.trafficState.AlgorithmType
 
 class CreateLightPhaseStrategyRequest {
-    public lateinit var algorithm: AlgorithmType
-    public var turnLength: Int = 0
-    public lateinit var intersections: List<IntersectionId>
+    lateinit var algorithm: AlgorithmType
+    var turnLength: Int = 0
+    lateinit var intersections: List<IntersectionId>
 }
