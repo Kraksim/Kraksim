542e9f8fcdb352e4884b43182208eee44b16c455
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-10-12 19:38:16 +0200

    Fixup swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,7 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.4.2.Final")
     kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
     implementation("com.google.code.findbugs:jsr305:3.0.2")
-    implementation("io.springfox:springfox-swagger2:3.0.0")
-    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+    implementation("org.springdoc:springdoc-openapi-ui:1.5.11")
 }
 
 kapt {
