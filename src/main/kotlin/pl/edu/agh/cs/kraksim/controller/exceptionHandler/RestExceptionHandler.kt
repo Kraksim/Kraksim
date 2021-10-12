14460d5ea5e5f5d8c4c08c3c3a2747248005ea76
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-10-12 20:00:38 +0200

    Controller my bad

--- src/main/kotlin/pl/edu/agh/cs/kraksim/controller/exceptionHandler/RestExceptionHandler.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/controller/exceptionHandler/RestExceptionHandler.kt
@@ -4,18 +4,17 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.context.request.WebRequest
 import pl.edu.agh.cs.kraksim.api.exception.ObjectNotFoundException
 
 @ControllerAdvice
 class RestExceptionHandler {
     @ExceptionHandler(value = [ObjectNotFoundException::class])
-    protected fun handleNotFound(ex: RuntimeException, request: WebRequest?): ResponseEntity<Any> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body<Any>(ex.toString())
+    protected fun handleNotFound(ex: ObjectNotFoundException): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.toString())
     }
 
     @ExceptionHandler(value = [Exception::class])
-    protected fun handleDefault(ex: RuntimeException, request: WebRequest?): ResponseEntity<Any> {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body<Any>(ex.toString())
+    protected fun handleDefault(ex: Exception): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.toString())
     }
 }
