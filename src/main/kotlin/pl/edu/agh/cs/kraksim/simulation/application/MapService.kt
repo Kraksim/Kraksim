4a1e881ee4334533fc18986bcfd4a41b2c807f9e
miloszgalas <milosz.galas2@gmail.com>
2021-10-22 22:20:00 +0200

    map creation (#26)
    * map creation
    
    * fix whitespaces
    
    * extract to mapper
    
    * fix
    
    * fix

---
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/MapService.kt
@@ -0,0 +1,10 @@
+package pl.edu.agh.cs.kraksim.simulation.application
+
+import org.springframework.stereotype.Service
+import pl.edu.agh.cs.kraksim.simulation.domain.*
+
+@Service
+class MapService(val mapper: DTOToEntityMapper) {
+
+    fun createMap(mapDTO: MapDTO): MapEntity = mapper.createMap(mapDTO)
+}
---
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/web/MapController.kt
@@ -0,0 +1,32 @@
+package pl.edu.agh.cs.kraksim.simulation.web
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import pl.edu.agh.cs.kraksim.simulation.application.MapMapper
+import pl.edu.agh.cs.kraksim.simulation.application.MapService
+import pl.edu.agh.cs.kraksim.simulation.db.MapRepository
+import pl.edu.agh.cs.kraksim.simulation.domain.*
+
+@RequestMapping("/map")
+@RestController
+class MapController(
+    val mapper: MapMapper,
+    val mapRepository: MapRepository,
+    val service: MapService
+) {
+
+    @PostMapping
+    fun createMap(@RequestBody mapDTO: MapDTO) {
+        mapRepository.save(service.createMap(mapDTO))
+    }
+
+    @GetMapping
+    fun getMap(@RequestParam id: Long): ResponseEntity<MapDTO> {
+        return ResponseEntity.ok(mapper.convertToDto(mapRepository.getById(id)))
+    }
+
+    @GetMapping("/ids")
+    fun getAllIds(): ResponseEntity<List<Long>> {
+        return ResponseEntity.ok(mapRepository.findAll().map { it.id })
+    }
+}
