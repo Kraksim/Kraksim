69aef77adfa5f302e5992044c42da8cb9ec9287f
milosz <milosz.galas2@gmail.com>
2021-10-22 22:06:38 +0200

    fix

--- src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/MapService.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/MapService.kt
@@ -6,8 +6,6 @@ import pl.edu.agh.cs.kraksim.simulation.domain.*
 @Service
 class MapService(val mapper: DTOToEntityMapper) {
 
-    fun createMap(mapDTO: MapDTO): MapEntity {
-        return mapper.createMap(mapDTO)
-    }
+    fun createMap(mapDTO: MapDTO): MapEntity = mapper.createMap(mapDTO)
 
 }
