153eee0f686eddb917efbfdc74e124efa87bfe66
Wiktor Kamiński <wkaminski@qualtrics.com>
2021-10-24 17:00:07 +0200

    lint

--- src/main/kotlin/pl/edu/agh/cs/kraksim/common/configuration/CorsConfiguration.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/common/configuration/CorsConfiguration.kt
@@ -5,12 +5,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @EnableWebMvc
 @Configuration
 class CorsConfiguration : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
     }
 }
