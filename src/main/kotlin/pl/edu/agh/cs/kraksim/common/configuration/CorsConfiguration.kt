e58069e537b9863dfb3b9b3f2f079ee95bd84daa
Wiktor Kamiński <kamykikamienie@gmail.com>
2021-10-25 18:41:27 +0200

    Add cors config (#28)
    * added cors config
    
    * lint
    
    Co-authored-by: Wiktor Kamiński <wkaminski@qualtrics.com>

---
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/common/configuration/CorsConfiguration.kt
@@ -0,0 +1,15 @@
+package pl.edu.agh.cs.kraksim.common.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@EnableWebMvc
+@Configuration
+class CorsConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+}
