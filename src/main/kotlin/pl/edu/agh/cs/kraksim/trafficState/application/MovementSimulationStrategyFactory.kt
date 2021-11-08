9019d76b7f372f1121fe88c931db04193ca90e10
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-11-08 21:16:57 +0100

    Fixed flaky test and added entity mapping

--- src/main/kotlin/pl/edu/agh/cs/kraksim/trafficState/application/MovementSimulationStrategyFactory.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/trafficState/application/MovementSimulationStrategyFactory.kt
@@ -2,6 +2,7 @@ package pl.edu.agh.cs.kraksim.trafficState.application
 
 import org.springframework.stereotype.Component
 import pl.edu.agh.cs.kraksim.common.random.TrueRandomProvider
+import pl.edu.agh.cs.kraksim.core.movementStrategyUseCase.multilaneNagel.MultiLaneNagelMovementSimulationStrategy
 import pl.edu.agh.cs.kraksim.core.movementStrategyUseCase.nagel.NagelMovementSimulationStrategy
 import pl.edu.agh.cs.kraksim.trafficState.domain.entity.MovementSimulationStrategyEntity
 import pl.edu.agh.cs.kraksim.trafficState.domain.entity.MovementSimulationStrategyType
@@ -11,11 +12,20 @@ import pl.edu.agh.cs.kraksim.trafficState.domain.entity.RandomProviderType
 class MovementSimulationStrategyFactory {
     fun from(entity: MovementSimulationStrategyEntity) = when (entity.type) {
         MovementSimulationStrategyType.NAGEL_SCHRECKENBERG -> createNagelStrategy(entity)
+        MovementSimulationStrategyType.MULTI_LANE_NAGEL_SCHRECKENBERG -> createMultiLaneNagelStrategy(entity)
     }
 
     private fun createNagelStrategy(entity: MovementSimulationStrategyEntity): NagelMovementSimulationStrategy {
         val randomProvider = createRandomProvider(entity)
         return NagelMovementSimulationStrategy(
+            randomProvider,
+            entity.maxVelocity
+        )
+    }
+
+    private fun createMultiLaneNagelStrategy(entity: MovementSimulationStrategyEntity): MultiLaneNagelMovementSimulationStrategy {
+        val randomProvider = createRandomProvider(entity)
+        return MultiLaneNagelMovementSimulationStrategy(
             randomProvider,
             entity.maxVelocity
         )
