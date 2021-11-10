e8f889b2640ea6bc8b52075cf07d805cdf474621
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-11-10 20:14:14 +0100

    Fixed flaky test and added entity mapping (#32)

--- src/test/kotlin/pl/edu/agh/cs/kraksim/core/movementStrategyUseCase/multilaneNagel/MultiLaneNagelMovementSimulationStrategyTest.kt
+++ src/test/kotlin/pl/edu/agh/cs/kraksim/core/movementStrategyUseCase/multilaneNagel/MultiLaneNagelMovementSimulationStrategyTest.kt
@@ -2,6 +2,7 @@ package pl.edu.agh.cs.kraksim.core.movementStrategyUseCase.multilaneNagel
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import pl.edu.agh.cs.kraksim.common.MockRandomProvider
 import pl.edu.agh.cs.kraksim.common.getLane
 import pl.edu.agh.cs.kraksim.common.testMultiLaneNagelMovementSimulationStrategy
 import pl.edu.agh.cs.kraksim.core.movementStrategyUseCase.multilaneNagel.utils.getTwoRoadConnectedWithIntersectionMultiLaneSimulationState
@@ -20,7 +21,7 @@ internal class MultiLaneNagelMovementSimulationStrategyTest {
             gps = GPS(route = listOf(state.roads[0]!!, state.roads[1]!!), type = GPSType.DIJKSTRA_ROAD_LENGTH).apply { popNext() }
         )
         car.moveToLaneFront(state.getLane(roadId = 0, laneId = 11), 0)
-        val strategy = testMultiLaneNagelMovementSimulationStrategy()
+        val strategy = testMultiLaneNagelMovementSimulationStrategy(randomProvider = MockRandomProvider(booleanToReturn = false, randomElementIndex = 1))
 
         // when
         strategy.step(state)
