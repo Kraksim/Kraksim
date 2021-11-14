0131acbd4dfc81416be0576cfc0fd054e446fcf4
Grzegorz Poreba <gporeba@atlassian.com>
2021-11-14 17:11:58 +0100

    Added strategy validation

--- src/main/kotlin/pl/edu/agh/cs/kraksim/common/exception/InvalidSimulationStateConfigurationException.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/common/exception/InvalidSimulationStateConfigurationException.kt
@@ -1,7 +1,4 @@
 package pl.edu.agh.cs.kraksim.common.exception
 
-class InvalidSimulationStateConfigurationException(val exceptions: List<Exception>) :
-    Exception(exceptions.mapExceptionsToMessage())
-
-private fun List<Exception>.mapExceptionsToMessage() =
-    map { it.message }.joinToString(separator = ", ")
+class InvalidSimulationStateConfigurationException(val exceptions: List<String>) :
+    Exception(exceptions.joinToString(separator = ", \n"))
