ecfb5bf96dd49f8341f0cae1747d8d59dc751afe
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-11-18 20:26:51 +0100

    Added basic validation (#42)
    * Added basic validation
    
    * added logging
    
    Co-authored-by: Grzegorz Poreba <gporeba@atlassian.com>

--- src/main/kotlin/pl/edu/agh/cs/kraksim/trafficState/domain/request/CreateInitialSimulationStateRequest.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/trafficState/domain/request/CreateInitialSimulationStateRequest.kt
@@ -1,5 +1,8 @@
 package pl.edu.agh.cs.kraksim.trafficState.domain.request
 
-class CreateInitialSimulationStateRequest {
-    lateinit var gatewaysStates: List<CreateGatewayStateRequest>
-}
+import javax.validation.constraints.NotEmpty
+
+class CreateInitialSimulationStateRequest(
+    @field:NotEmpty
+    var gatewaysStates: List<CreateGatewayStateRequest>
+)
