378a7222a99add2131f57a13ab5eb87131ece0a4
Grzegorz Poreba <gporeba@atlassian.com>
2021-11-26 00:58:29 +0100

    Added basic map info for creating graph

--- src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/MapService.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/MapService.kt
@@ -4,8 +4,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import pl.edu.agh.cs.kraksim.common.exception.ObjectNotFoundException
 import pl.edu.agh.cs.kraksim.simulation.db.MapRepository
-import pl.edu.agh.cs.kraksim.simulation.domain.MapEntity
-import pl.edu.agh.cs.kraksim.simulation.domain.BasicMapInfoDTO
+import pl.edu.agh.cs.kraksim.simulation.domain.*
 import pl.edu.agh.cs.kraksim.simulation.web.request.CreateMapRequest
 
 @Service
@@ -23,7 +22,55 @@ class MapService(
         return mapRepository.findByIdOrNull(id) ?: throw ObjectNotFoundException("Couldn't find map with id = $id")
     }
 
+    fun getBasicById(id: Long): BasicMapInfoDTO {
+        val map = mapRepository.findByIdOrNull(id) ?: throw ObjectNotFoundException("Couldn't find map with id = $id")
+        return getBasicMapInfoDTO(map)
+    }
+
     fun getAllMapsBasicInfo(): List<BasicMapInfoDTO> {
-        return mapRepository.getAllMapsBasicInfo()
+        val all = mapRepository.findAll()
+        return all.map { entity -> getBasicMapInfoDTO(entity) }
+    }
+
+    private fun getBasicMapInfoDTO(entity: MapEntity) = BasicMapInfoDTO(
+        type = entity.type,
+        name = entity.name,
+        id = entity.id,
+        nodes = calculateNodes(entity),
+        edges = calculateEdges(entity)
+    )
+
+    private fun calculateEdges(entity: MapEntity): List<BasicEdgeDto> {
+        val fromMap = HashMap<Long, RoadNodeEntity>()
+        val toMap = HashMap<Long, RoadNodeEntity>()
+
+        entity.roadNodes.forEach { node ->
+            node.endingRoads.forEach { road ->
+                toMap[road.id] = node
+            }
+
+            node.startingRoads.forEach { road ->
+                fromMap[road.id] = node
+            }
+        }
+
+        return entity.roads.map { e ->
+            BasicEdgeDto(
+                from = fromMap[e.id]!!.id,
+                to = toMap[e.id]!!.id,
+                lanesThickness = e.lanes.size,
+            )
+        }
+    }
+
+    private fun calculateNodes(entity: MapEntity): List<BasicRoadNodeDto> {
+        return entity.roadNodes.map {
+            BasicRoadNodeDto(
+                name = it.name,
+                type = it.type,
+                id = it.id,
+                position = PositionDTO(x = it.position.x, y = it.position.y)
+            )
+        }
     }
 }
--- src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/db/MapRepository.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/db/MapRepository.kt
@@ -1,14 +1,8 @@
 package pl.edu.agh.cs.kraksim.simulation.db
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import pl.edu.agh.cs.kraksim.simulation.domain.BasicMapInfoDTO
 import pl.edu.agh.cs.kraksim.simulation.domain.MapEntity
 
 @Repository
-interface MapRepository : JpaRepository<MapEntity, Long> {
-
-    @Query("SELECT new pl.edu.agh.cs.kraksim.simulation.domain.BasicMapInfoDTO(e.type, e.name, e.id) from MapEntity e")
-    fun getAllMapsBasicInfo(): List<BasicMapInfoDTO>
-}
+interface MapRepository : JpaRepository<MapEntity, Long>
