14362a76f2d5820f4a683311f44a8a54694c4316
Grzegorz Poreba <gporeba@atlassian.com>
2021-12-04 01:36:58 +0100

    Validation - if road end can be reached with this lane config and some other constraints

--- src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/RequestToEntityMapper.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/simulation/application/RequestToEntityMapper.kt
@@ -3,6 +3,7 @@ package pl.edu.agh.cs.kraksim.simulation.application
 import org.apache.logging.log4j.LogManager
 import org.springframework.stereotype.Component
 import pl.edu.agh.cs.kraksim.common.exception.InvalidMapConfigurationException
+import pl.edu.agh.cs.kraksim.common.getRepeatsBy
 import pl.edu.agh.cs.kraksim.simulation.domain.*
 import pl.edu.agh.cs.kraksim.simulation.web.request.*
 import pl.edu.agh.cs.kraksim.trafficLight.domain.TrafficLightPhase
@@ -168,44 +169,166 @@ class RequestToEntityMapper {
 
     private fun validateCreateMap(createMapRequest: CreateMapRequest) {
         val exceptions = listOf(
-            createMapRequest.roadNodes.mapNotNull { validate(it) },
-            validateRoads(createMapRequest.roads)
+            createMapRequest.roadNodes.map { validateRoadNode(it) }.flatten(),
+            validateRoads(createMapRequest.roads),
+            validateAtLeastOneGateway(createMapRequest.roadNodes),
+            createMapRequest.roadNodes.getRepeatsBy { it.name }.map { "Bad intersection configuration name='${it.key}' - road node names have to be unique" }
+
         ).flatten()
         if (exceptions.isNotEmpty()) {
             throw InvalidMapConfigurationException(exceptions)
         }
     }
 
+    private fun validateAtLeastOneGateway(roadNodes: List<CreateRoadNodeRequest>): List<String> {
+        return if (roadNodes.none { it.type == RoadNodeType.GATEWAY })
+            listOf("Map has to contain at least one gateway")
+        else
+            emptyList()
+    }
+
     private fun validateRoads(roads: List<CreateRoadRequest>): List<String> {
         val errors = ArrayList<String>()
-        val areRoadsIdUnique = roads.distinctBy { it.id }.size == roads.size
-        if (!areRoadsIdUnique) {
-            errors.add("Road ids must be unique")
+        val repeatedRoadIds = roads.getRepeatsBy { it.id }
+        if (repeatedRoadIds.isNotEmpty()) {
+            errors.add("Road ids must be unique (${repeatedRoadIds.keys})")
         }
+
+        val repeatedRoadNames = roads.getRepeatsBy { it.name }
+        if (repeatedRoadNames.isNotEmpty()) {
+            errors.add("Road names must be unique (${repeatedRoadNames.keys})")
+        }
+
         val lanes = roads.flatMap { it.lanes }
-        val areLaneIdsUnique = lanes.distinctBy { it.id }.size == lanes.size
-        if (!areLaneIdsUnique) {
-            errors.add("Lane ids must be unique")
+        val repeatedLaneIds = lanes.getRepeatsBy { it.id }
+        if (repeatedLaneIds.isNotEmpty()) {
+            errors.add("Lane ids must be unique (${repeatedLaneIds.keys})")
         }
+
+        val laneErrors = roads.flatMap { validateLanes(it) }
+        errors.addAll(laneErrors)
         return errors
     }
 
-    private fun validate(request: CreateRoadNodeRequest): String? {
+    private fun validateLanes(road: CreateRoadRequest): List<String> {
+        val errorPrefix = "Road id=${road.id} has incorrect lane configuration -"
+
+        val minIndexFromLeft = road.lanes.minOf { it.indexFromLeft }
+        val maxIndexFromLeft = road.lanes.maxOf { it.indexFromLeft }
+        val errors = ArrayList<String>()
+
+        errors.addAll(road.lanes.filter { it.startingPoint < 0 || it.endingPoint > road.length }
+            .map { "$errorPrefix lane id=${it.id} has incorrect starting or ending point" })
+
+        if (minIndexFromLeft != 0)
+            errors.add("$errorPrefix lane index from left has to start from 0")
+
+        if (maxIndexFromLeft != road.lanes.size - 1)
+            errors.add("$errorPrefix lane index from left have to be in sequence from 0 to number of lanes - 1")
+
+        val driveError = validateIfRoadCanBeDrivenThrough(road, errorPrefix)
+        if (driveError != null) {
+            errors.add(driveError)
+        }
+
+        return errors
+    }
+
+    private fun validateIfRoadCanBeDrivenThrough(road: CreateRoadRequest, errorPrefix: String): String? {
+        val startingLanes = road.lanes.filter { it.startingPoint == 0 }
+        if (startingLanes.isEmpty())
+            return "$errorPrefix no lane with starting point = 0"
+
+        val canBeDrivenThrough =
+            startingLanes.all {
+                tryReachEndRecc(
+                    it,
+                    changeLanePosition = 0,
+                    road.lanes,
+                    road.length,
+                    visitedLanes = emptyList()
+                )
+            } // we must assume that from every lane starting where road start we can reach end
+        // because while simulating we can't distinguish which starting lane will lead to road end
+        if (!canBeDrivenThrough)
+            return "$errorPrefix can't reach end of the road with this lane configuration"
+        return null
+    }
+
+    /**
+     * we keep changeLanePosition to invalidate this shape of lanes:
+     *          |
+     *  | | |   |
+     *  |   | | |
+     *  |
+     */
+    private fun tryReachEndRecc(
+        it: CreateLaneRequest,
+        changeLanePosition: Int,
+        lanes: List<CreateLaneRequest>,
+        length: Int,
+        visitedLanes: List<CreateLaneRequest>
+    ): Boolean {
+        if (it.endingPoint == length)
+            return true
+
+        val currentIndex = it.indexFromLeft
+        val currentRange = changeLanePosition..it.endingPoint
+        val leftLane = lanes.find { it.indexFromLeft == currentIndex - 1 }
+        val rightLane = lanes.find { it.indexFromLeft == currentIndex + 1 }
+
+        if (changeLaneAndTryReachEndRecc(leftLane, visitedLanes, currentRange, lanes, length, it))
+            return true
+
+        if (changeLaneAndTryReachEndRecc(rightLane, visitedLanes, currentRange, lanes, length, it))
+            return true
+
+        return false;
+    }
+
+    private fun changeLaneAndTryReachEndRecc(
+        targetLane: CreateLaneRequest?,
+        visitedLanes: List<CreateLaneRequest>,
+        currentRange: IntRange,
+        lanes: List<CreateLaneRequest>,
+        length: Int,
+        it: CreateLaneRequest
+    ): Boolean {
+        if (targetLane != null && !visitedLanes.contains(targetLane)) {
+            val rightRange = targetLane.startingPoint..targetLane.endingPoint
+            val rangeWhereNextToEachOther = currentRange.intersect(rightRange)
+            if (rangeWhereNextToEachOther.isNotEmpty()) {
+                val resultRight =
+                    tryReachEndRecc(targetLane, rangeWhereNextToEachOther.first(), lanes, length, visitedLanes + it)
+                if (resultRight)
+                    return true
+            }
+        }
+        return false
+    }
+
+    private fun validateRoadNode(request: CreateRoadNodeRequest): List<String> {
         val initialTurnDirectionsSpecified = request.turnDirections != null && request.turnDirections.isNotEmpty()
         val overrideTurnDirections = request.overrideTurnDirectionsTurnEverywhere
+        val errors = ArrayList<String>()
 
         if (request.type == RoadNodeType.INTERSECTION) {
             if (!initialTurnDirectionsSpecified && !overrideTurnDirections)
-                return "Bad intersection configuration name='${request.name}' - specify either turn directions or turn on flag to override everywhere"
+                errors.add("Bad intersection configuration name='${request.name}' - specify either turn directions or turn on flag to override everywhere")
             else if (initialTurnDirectionsSpecified && overrideTurnDirections) {
                 log.warn("Specified turn directions for node name='${request.name}' will be ignored!")
             }
+
+            if (request.startingRoadsIds.isEmpty()) {
+                errors.add("Bad intersection configuration name='${request.name}' - intersection must have at least one road starting from it")
+            }
+
         } else {
             if (initialTurnDirectionsSpecified || overrideTurnDirections) {
                 log.warn("Even through node name='${request.name}' type is Gateway there were turnDirections or overrideTurnDirectionsTurnEverywhere specified - they will be ignored!")
             }
         }
-        return null
+        return errors
     }
 
     private fun getTurnDirections(
