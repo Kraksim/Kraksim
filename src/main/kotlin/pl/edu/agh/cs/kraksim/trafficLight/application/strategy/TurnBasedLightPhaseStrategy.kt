e6acd62c66d15e39e0416aee567b22b678729716
Grzegorz Poręba <grzegorz.poreba.73@gmail.com>
2021-12-08 17:56:57 +0100

    Fixed bug not switching lights when only 1 road end in that intersection (#59)
    * Fixed bug not switching lights when only 1 road end in that intersection
    
    * linter
    
    * revew
    
    Co-authored-by: Grzegorz Poreba <gporeba@atlassian.com>

--- src/main/kotlin/pl/edu/agh/cs/kraksim/trafficLight/application/strategy/TurnBasedLightPhaseStrategy.kt
+++ src/main/kotlin/pl/edu/agh/cs/kraksim/trafficLight/application/strategy/TurnBasedLightPhaseStrategy.kt
@@ -46,8 +46,16 @@ class TurnBasedLightPhaseStrategy(
     private fun switchLights(intersection: Intersection) {
         val roadsCount = intersection.endingRoads.size
 
+        val lightPhasesOfLanesGroupedByRoad = intersection.getLightPhasesOfLanesGroupedByRoad()
+        if (lightPhasesOfLanesGroupedByRoad.size == 1) {
+            lightPhasesOfLanesGroupedByRoad.forEach {
+                it.forEach { phase -> phase.state = GREEN }
+            }
+            return
+        }
+
         val phasesToChange =
-            intersection.getLightPhasesOfLanesGroupedByRoad()
+            lightPhasesOfLanesGroupedByRoad
                 .onEach { lanes -> lanes.forEach { it.phaseTime++ } }
                 .filter { roadPhases -> roadPhases[0].phaseTime == roadPhases[0].period }
 
